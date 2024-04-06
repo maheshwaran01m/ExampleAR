14248d0826743b78e1edb0d921106e583f75a70f
MAHESHWARAN <maheshwaran99m@gmail.com>
2024-04-06 20:18:55 +0530

    Source  - Updated AR view

--- ExampleAR/Sources/ViewController.swift
+++ ExampleAR/Sources/ViewController.swift
@@ -6,17 +6,47 @@
 //
 
 import UIKit
+import ARKit
 
 class ViewController: UIViewController {
+  
+  private var sceneView: CustomARView?
 
+  // MARK: - Override Methods
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     setupView()
   }
+  
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    setupView()
+  }
+  
+  override func viewWillDisappear(_ animated: Bool) {
+    super.viewWillDisappear(animated)
+    sceneView?.session.pause()
+  }
+  
+  // MARK: - Views
   
   private func setupView() {
     title = "Home"
     view.backgroundColor = .systemBackground
     navigationController?.navigationBar.prefersLargeTitles = true
   }
+  
+  private func setupARView() {
+    sceneView = CustomARView(frame: view.bounds)
+    view.addSubView(sceneView)
+    sceneView?.delegate = self
+    
+    sceneView?.showsStatistics = true
+    
+    let config = ARWorldTrackingConfiguration()
+    sceneView?.session.run(config)
+  }
 }
+
+extension ViewController: ARSCNViewDelegate { }
